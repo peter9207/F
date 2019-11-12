f21d906129ef76c96024e0c2a0e083cd9194f0c7
Peter Zhang <peter920730@gmail.com>
2019-11-11 23:01:02 -0800

    add interface

--- S/predictors/simple.go
+++ S/predictors/simple.go
@@ -4,16 +4,20 @@ import (
 	"github.com/peter9207/F/S/average"
 )
 
-type Predictor struct {
+type Predictor interface {
+	Predict(data []float64) bool
+}
+
+type SimplePredictor struct {
 	Score      int64
 	windowSize int64
 }
 
-func SimpleRolling(bucketSize int64) (p *Predictor) {
-	return &Predictor{windowSize: bucketSize}
+func SimpleRolling(bucketSize int64) (p *SimplePredictor) {
+	return &SimplePredictor{windowSize: bucketSize}
 }
 
-func (p *Predictor) Predict(data []float64) (b bool) {
+func (p *SimplePredictor) Predict(data []float64) (b bool) {
 
 	averages := average.Rolling(data, p.windowSize)
 
