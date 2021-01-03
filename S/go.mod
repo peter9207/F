3bd04f5569edcb0f15b2106c50c2fabf4ea3b20e
Peter Zhang <peter920730@gmail.com>
2021-01-02 16:56:06 -0800

    used modules

---
+++ S/go.mod
@@ -0,0 +1,10 @@
+module example.com/m
+
+go 1.16
+
+require (
+	github.com/onsi/ginkgo v1.14.2
+	github.com/onsi/gomega v1.10.4
+	github.com/peter9207/F v0.0.0-20201227212718-a8b3213b3e0d
+	github.com/spf13/cobra v1.1.1
+)
--- W/javascript/ast.go
+++ W/javascript/ast.go
@@ -4,26 +4,84 @@ import (
 	"fmt"
 )
 
-type Node map[string]interface{}
+// type Node map[string]interface{}
 
-func (e *Node) Type() string {
-
-	t, ok := e["type"].(string)
-	if !ok {
-		fmt.Println("[Warning] expression has no field type")
-	}
-	return t
+type Node struct {
+	Value    string
+	Type     string `json:"type"`
+	meta     map[string]string
+	Children []Node
 }
 
-func parse() {
-	t := map[string]interface{}{
-		"testing": 123,
+type Program struct {
+	Body []Statement `json:"body"`
+}
+
+type BlockStatement struct {
+	Body []Statement `json:"body"`
+}
+
+type ExpressionStatement struct {
+}
+
+type Identifier struct {
+	name string `json:"name"`
+}
+
+type Literal struct {
+	raw string `json:"raw"`
+}
+type VariableDeclarator struct {
+	id Identifier `json:"id"`
+}
+type VariableDeclaration struct {
+	Kind        string             `json:"kind"`
+	Declaration VariableDeclarator `json:"declarator"`
+}
+
+type BinaryExpression struct {
+}
+
+type IfStatement struct {
+	test       string
+	subsequent string
+	alternate  string
+}
+
+type FunctionDeclaration struct {
+}
+
+func toNode(root map[string]interface{}) (node *Node) {
+
+	node = &Node{}
+
+	for k, v := range root {
+
+		switch k {
+		case "type":
+			node.Type = v
+		}
+
 	}
 
-	n := Node{}
-	for k, v := range t {
-		n[k] = v
-	}
+	// switch root.(type) {
+	// case []interface{}:
+	// 	for _, v := range root {
+
+	// 	}
+	// case map[string]interface{}:
+	// 	for k, v := range root {
+
+	// 		node[k] = toNode(v)
+	// 	}
+	// 	return
+	// default:
+	// }
+}
+
+// create data nodes
+
+func listToNodes(l []interface{}) (nodes []Node) {
 
 }
 
